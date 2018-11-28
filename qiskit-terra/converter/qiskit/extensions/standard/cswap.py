00e1ece500ad8706be99e9bfce35dae5afb91bdc
Mahita Nagabhiru <mnagabh@ncsu.edu>
2018-11-28 17:19:27 -0500

    cswap format change

--- qiskit-terra/converter/qiskit/extensions/standard/cswap.py
+++ qiskit-terra/converter/qiskit/extensions/standard/cswap.py
@@ -30,7 +30,9 @@ class FredkinGate(Gate):
         """Reapply this gate to corresponding qubits in circ."""
         self._modifiers(circ.cswap(self.qargs[0], self.qargs[1], self.qargs[2]))
 
+
 def cswap(self, ctl1, tgt1, tgt2):
+    ################# Recursively Call across all bits #########################
     if isinstance(ctl1, QuantumRegister) and \
        isinstance(tgt1, QuantumRegister) and \
        isinstance(tgt2, QuantumRegister) and \
@@ -38,53 +40,57 @@ def cswap(self, ctl1, tgt1, tgt2):
         for i in range(ctl1.size):
             self.cswap((ctl1, i), (tgt1, i), (tgt2, i))
         return None
-    ############################## Write Dwave CSWAP ##################################
+
+############################## Write Dwave CSWAP ##################################
     elif isinstance(ctl1, QuantumRegister) and \
        isinstance(tgt1, QuantumRegister) and \
        isinstance(tgt2, QuantumRegister):
        raise("All registers must be of the same size")
-
-    """Apply CSWAP from ctl1 to tgt1 and tgt2."""
     import os
     import sys
     import __main__ as main
 
-    ctl1name = ctl1[0].name + "_" + str(ctl1[1])
-    tgt1name = tgt1[0].name + "_" + str(tgt1[1])
-    tgt2name = tgt2[0].name + "_" + str(tgt2[1])
+    ctl1name = ctl1[0].name + '_' + str(ctl1[1])
+    tgt1name = tgt1[0].name + '_' + str(tgt1[1])
+    tgt2name = tgt2[0].name + '_' + str(tgt2[1])
 
     filename = main.__file__.split(".")[0] + "_dwave.py"
     with open(filename, "a") as f:
-
-        f.write("####################################################\n"\
-                "## CSWAP - control1: {0} target1: {1} target2: {2} ##\n"\
-                "####################################################\n\n"\
-                "if \'{0}\' not in globals():\n"\
-                "    {0}=0\n"\
-                "if \'{1}\' not in globals():\n"\
-                "    {1}=0\n"\
-                "if \'{2}\' not in globals():\n"\
-                "    {2}=0\n\n"\
-                "qubit_weights = {{\'{1}\' : 1, \'{2}\' : 1, \'out{1}\' : 1, \'out{2}\' : 1, \'a\' : 6, \'b\' : 6}}\n"\
-                "binding_weights = {{(\'{0}\', \'out{1}\') : 2, (\'{0}\', \'out{2}\') : 2, (\'{0}\', \'a\') : -4, (\'{0}\', \'b\') : -4, (\'{1}\', \'out{1}\') : -2, (\'{1}\', \'a\') : 2, (\'{1}\', \'b\') : -2, (\'{2}\', \'out{2}\') : -2, (\'{2}\', \'a\') : -2, (\'{2}\', \'b\') : 2, (\'out{1}\', \'a\') : -4, (\'out{2}\', \'b\') : -4}}\n"\
-                "bqm = dimod.BinaryQuadraticModel(qubit_weights, binding_weights, 0, dimod.BINARY)\n"\
-                "sampler = dimod.ExactSolver()\n"\
-                "response = sampler.sample(bqm)\n\n"\
-                "for sample, energy in response.data(['sample', 'energy']):\n"\
-                "    if sample[\'{0}\']=={0} and sample[\'{1}\']=={1} and sample[\'{2}\']=={2} and int(energy)==0:\n"\
-                "        {0}=sample[\'{0}\']\n"\
-                "        {1}=sample[\'out{1}\']\n"\
-                "        {2}=sample[\'out{2}\']\n"\
-                "        tgt1_before = sample[\'{1}\']\n"\
-                "        tgt2_before = sample[\'{2}\']\n"\
-                "        #print(sample, energy)\n"\
-                "        break\n\n"\
-                "print(\"######################################################\")\n"\
-                "print(\"CSWAP operation on {0} (control1), {1} (target1) and {2} (target2):\")\n"\
-                "print(\"    in:  {0}={{0}}, {1}={{1}}, {2}={{2}}\".format({0},tgt1_before,tgt2_before))\n"\
-                "print(\"    out: {0}={{0}}, {1}={{1}}, {2}={{2}}\".format({0},{1},{2}))\n"\
-                "print(\"######################################################\")\n"\
-                "print(\"\\n\\n\\n\")\n\n\n".format(ctl1name, tgt1name, tgt2name))
+        f.write("#" * 80 + "\n")
+        f.write("## CCNOT - control1: {0} target1: {1} target2: {2} ##\n".format(ctl1name,tgt1name,tgt2name))
+        f.write("#" * 80 + "\n")
+        f.write("if \'{0}\' not in globals():\n".format(ctl1name,tgt1name,tgt2name))
+        f.write("    {0}=0\n".format(ctl1name,tgt1name,tgt2name))
+        f.write("if \'{1}\' not in globals():\n".format(ctl1name,tgt1name,tgt2name))
+        f.write("    {1}=0\n".format(ctl1name,tgt1name,tgt2name))
+        f.write("if \'{2}\' not in globals():\n".format(ctl1name,tgt1name,tgt2name))
+        f.write("    {2}=0\n\n".format(ctl1name,tgt1name,tgt2name))
+        f.write("qubit_weights = {{\'{1}\' : 1, \'{2}\' : 1, \'out{1}\' : 1, \'out{2}\' : 1, \'a\' : 6, \'b\' : 6}}\n")
+        f.write("binding_weights = {{(\'{0}\', \'out{1}\') : 2,
+                (\'{0}\', \'out{2}\') : 2, (\'{0}\', \'a\') : -4,
+                (\'{0}\', \'b\') : -4, (\'{1}\', \'out{1}\') : -2,
+                (\'{1}\', \'a\') : 2, (\'{1}\', \'b\') : -2, (\'{2}\',
+                \'out{2}\') : -2, (\'{2}\', \'a\') : -2, (\'{2}\',
+                \'b\') : 2, (\'out{1}\', \'a\') : -4, (\'out{2}\',
+                \'b\') : -4}}\n")
+        f.write("bqm = dimod.BinaryQuadraticModel(qubit_weights, binding_weights, 0, dimod.BINARY)\n")
+        f.write("sampler = dimod.ExactSolver()\n")
+        f.write("response = sampler.sample(bqm)\n\n")
+        f.write("for sample, energy in response.data(['sample', 'energy']):\n")
+        f.write("    if sample[\'{0}\']=={0} and sample[\'{1}\']=={1} and sample[\'{2}\']=={2} and int(energy)==0:\n")
+        f.write("        {0}=sample[\'{0}\']\n")
+        f.write("        {1}=sample[\'out{1}\']\n")
+        f.write("        {2}=sample[\'out{2}\']\n")
+        f.write("        tgt1_before = sample[\'{1}\']\n")
+        f.write("        tgt2_before = sample[\'{2}\']\n")
+        f.write("        #print(sample, energy)\n")
+        f.write("        break\n\n")
+        f.write("print(\"######################################################\")\n")
+        f.write("print(\"CSWAP operation on {0} (control1), {1} (target1) and {2} (target2):\")\n")
+        f.write("print(\"    in:  {0}={{0}}, {1}={{1}}, {2}={{2}}\".format({0},tgt1_before,tgt2_before))\n")
+        f.write("print(\"    out: {0}={{0}}, {1}={{1}}, {2}={{2}}\".format({0},{1},{2}))\n")
+        f.write("print(\"######################################################\")\n")
+        f.write("print(\"\\n\\n\\n\")\n\n\n".format(ctl1name,tgt1name, tgt2name)))
 
     ##################################################################################
     return None
