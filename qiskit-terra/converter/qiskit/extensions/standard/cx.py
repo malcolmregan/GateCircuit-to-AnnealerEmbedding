ba8c6a25f01d2bfce5410593705aac10efcf0d1e
Brody Eastwood <baeastwo@ncsu.edu>
2018-11-28 16:47:05 -0500

    Cleaned up cx

--- qiskit-terra/converter/qiskit/extensions/standard/cx.py
+++ qiskit-terra/converter/qiskit/extensions/standard/cx.py
@@ -55,39 +55,36 @@ def cx(self, ctl, tgt):
     import os
     import sys
     import __main__ as main
-    
+
     ctlname = ctl[0].name + '_' + str(ctl[1])
     tgtname = tgt[0].name + '_' + str(tgt[1])
 
     filename = main.__file__.split(".")[0] + "_dwave.py"
     with open(filename, "a") as f:
-        
-        f.write("###################################\n"\
-                "## CNOT - control: {0} target: {1} ##\n"\
-                "###################################\n\n"\
-                "if \'{0}\' not in globals():\n"\
-                "    {0}=0\n"\
-                "if \'{1}\' not in globals():\n"\
-                "    {1}=0\n\n"\
-                "bqm = dimod.BinaryQuadraticModel({{'{0}\' : 1, \'{1}\' : 1, \'out{1}\' : 1, \'anc\' : 4}}, {{(\'{0}\', \'{1}\') : 2, (\'{0}\', \'out{1}\') : -2, (\'{1}\', \'out{1}\') : "\
-                "-2, (\'{0}\', \'anc\') : -4, (\'{1}\', \'anc\') : -4, (\'out{1}\', \'anc\') : 4}}, 0, dimod.BINARY)\n"\
-                "sampler = dimod.ExactSolver()\n"\
-                "response = sampler.sample(bqm)\n\n"\
-                "for sample, energy in response.data(['sample', 'energy']):\n"\
-                "    if sample[\'{0}\']=={0} and sample[\'{1}\']=={1} and int(energy)==0:\n"\
-                "        {0}=sample[\'{0}\']\n"\
-                "        {1}=sample[\'out{1}\']\n"\
-                "        #ctl_before = sample[\'{0}\']\n"
-                "        tgt_before = sample[\'{1}\']\n"
-                "        #print(sample, energy)\n"\
-                "        break\n\n"\
-                "print(\"######################################################\")\n"\
-                "print(\"CNOT operation on {0} (control) and {1} (target):\")\n"\
-                "print(\"    in:  {0}={{0}}, {1}={{1}}\".format({0},tgt_before))\n"\
-                "print(\"    out: {0}={{0}}, {1}={{1}}\".format({0},{1}))\n"\
-                "print(\"######################################################\")\n"\
-                "print(\"\\n\\n\\n\")\n\n\n".format(ctlname,tgtname))
-                        
+        f.write("#" * 80 + "\n")
+        f.write("## CNOT - control: {0} target: {1} ##\n".format(ctlname,tgtname))
+        f.write("#" * 80 + "\n")
+        f.write("if \'{0}\' not in globals():\n".format(ctlname,tgtname))
+        f.write("    {0}=0\n".format(ctlname,tgtname))
+        f.write("if \'{1}\' not in globals():\n".format(ctlname,tgtname))
+        f.write("    {1}=0\n\n".format(ctlname,tgtname))
+        f.write("bqm = dimod.BinaryQuadraticModel({{'{0}\' : 1, \'{1}\' : 1, \'out{1}\' : 1, \'anc\' : 4}}, {{(\'{0}\', \'{1}\') : 2, (\'{0}\', \'out{1}\') : -2, (\'{1}\', \'out{1}\') : ".format(ctlname,tgtname))
+        f.write("-2, (\'{0}\', \'anc\') : -4, (\'{1}\', \'anc\') : -4, (\'out{1}\', \'anc\') : 4}}, 0, dimod.BINARY)\n".format(ctlname,tgtname))
+        f.write("sampler = dimod.ExactSolver()\n")
+        f.write("response = sampler.sample(bqm)\n\n")
+        f.write("for sample, energy in response.data(['sample', 'energy']):\n")
+        f.write("    if sample[\'{0}\']=={0} and sample[\'{1}\']=={1} and int(energy)==0:\n".format(ctlname,tgtname))
+        f.write("        {0}=sample[\'{0}\']\n".format(ctlname,tgtname))
+        f.write("        {1}=sample[\'out{1}\']\n".format(ctlname,tgtname))
+        f.write("        tgt_before = sample[\'{1}\']\n".format(ctlname,tgtname))
+        f.write("        break\n\n")
+        f.write("print('#' * 80)\n")
+        f.write("print(\"CNOT operation on {0} (control) and {1} (target):\")\n".format(ctlname,tgtname))
+        f.write("print(\"    in:  {0}={{0}}, {1}={{1}}\".format({0},tgt_before))\n".format(ctlname,tgtname))
+        f.write("print(\"    out: {0}={{0}}, {1}={{1}}\".format({0},{1}))\n".format(ctlname,tgtname))
+        f.write("print('#' * 80)\n")
+        f.write("print()\n")
+
     ##################################################################################
 
     return None
