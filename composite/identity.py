006d7aeaca39c304ad248eb1ebd324abf61fc1b2
Malcolm Regan <mjregan2@ncsu.edu>
2018-12-30 14:04:48 -0500

    l

---
+++ composite/identity.py
@@ -0,0 +1,132 @@
+import numpy as np
+from random  import randint, uniform, shuffle
+from solve_sys_multivar_ineq import *
+    
+
+system_inequalities = [
+# NOT 1
+#'w0 = -50',
+#'w1 = -50',
+'J01 = 500',
+# NOT 2
+#'w2 = -50',
+#'w3 = -50',
+'J23 = 500',
+# couplers btwn NOTs
+'J02 = 0',
+'J03 = 0',
+#'J12 = 0',
+'J13 = 0',
+# changed weights
+'w1 = -20',
+'w2 = -20',
+'J12 = -10',
+'w0 = -25',
+'w3 = -25']
+
+
+inputs = ['w0']
+outputs = ['w3']
+
+
+############################################
+############################################
+
+besttruecount = 0
+
+stop = False
+count = 0
+while stop == False:  
+    shuffle(system_inequalities)
+    symbols = solve(system_inequalities)
+    
+    correct = evaluate_sys(system_inequalities, symbols)
+    truecount = 0
+    falses = list()
+    for elem in correct:
+        #print(elem['valid'], "\t-", elem['inequality'])
+        if elem['valid']==True:
+            truecount = truecount + 1
+        else:
+            falses.append(elem['inequality'])
+    #print("\n")
+
+    if truecount >= besttruecount:
+        best = list()
+        for sym in symbols:
+            a = [sym.name, sym.value]
+            best.append(a)    
+        besttruecount = truecount
+        #print('best: ', besttruecount)
+        bestfalse = falses
+    if truecount == len(correct): 
+        stop = True
+    
+    count = count + 1
+
+    if count % 200 == 0:
+        s = input('Tried {} times. Stop? (y/n) '.format(count))
+        if s == 'y':
+            stop = True
+        elif s == 'n':
+            stop = False
+        else:
+            stop = False
+    print(count)
+
+print("\nSymbol Values: ")
+for entry in best:
+    print("\t",entry[0],"\t",entry[1])
+
+print('{} were true, {} were false:'.format(besttruecount,len(system_inequalities)-besttruecount))
+for i in range(len(bestfalse)):
+    print('False: ', bestfalse[i])
+
+import dimod
+
+qubit=list()
+qubitweight =list()
+coupler=list()
+couplerweight=list()
+offset = 0
+
+for sym in symbols:
+    if 'w' in sym.name:
+        qubit.append(sym.name)
+        qubitweight.append(sym.value)
+    if  'J' in sym.name:
+        numbers = sym.name[1:]
+        coupler.append(('w{}'.format(numbers[0]), 'w{}'.format(numbers[1])))
+        couplerweight.append(sym.value)
+    if sym.name == 'offset':
+        offset = sym.value
+
+qubit_weights = {q:w for q,w in zip(qubit, qubitweight)}
+coupler_weights = {c:w for c,w in zip(coupler,couplerweight)}
+
+print(qubit_weights,coupler_weights)
+
+bqm = dimod.BinaryQuadraticModel(qubit_weights, coupler_weights, offset, dimod.BINARY)
+sampler = dimod.ExactSolver()
+response = sampler.sample(bqm)
+
+groundstate = 1000000
+for sample, energy in response.data(['sample','energy']):
+    if energy<groundstate:
+        groundstate = round(energy,1)
+
+function = list()
+for sample, energy in response.data(['sample', 'energy']):
+    if round(energy,1) == groundstate:
+        print(sample, round(energy,1))
+        row = []
+        for inp in inputs:
+            row.append(sample[inp])
+        for outp in outputs:
+            row.append(sample[outp])
+        function.append(row)
+print('\n')
+
+function.sort()
+for i in range(len(function)):
+    print(function[i])
